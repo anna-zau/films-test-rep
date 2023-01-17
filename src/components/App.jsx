78885d140e1ac7579f5f3cb65703da464e578444
anna-zau <104685927+anna-zau@users.noreply.github.com>
2023-01-18 00:10:24 +0200

    ---

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+
+import { Home } from '../pages/Home';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 };
